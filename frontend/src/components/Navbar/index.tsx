f853f74a2f0443f2b557f043c54123d0678b99d5
Carlos Abraao <carloschavesnd@hotmail.com>
2022-01-14 10:15:01 -0300

    Navbar

---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,31 @@
+import './styles.css';
+import {ReactComponent as GithubIcon} from 'assets/img/Github.svg';
+
+function Navbar(){
+
+
+    return(
+
+
+
+        <header>
+        <nav className="container">
+            <div className="dsmovie-nav-content">
+                <h1>DSMovie</h1>
+                <a href="https://github.com/devsuperior" target="_blank" rel="noreferrer">
+                    <div className="dsmovie-contact-container">
+                        <GithubIcon />
+                        <p className="dsmovie-contact-link">/devsuperior</p>
+                    </div>
+                </a>
+            </div>
+        </nav>
+    </header>
+
+
+    );
+
+    
+}
+
+export default Navbar;
